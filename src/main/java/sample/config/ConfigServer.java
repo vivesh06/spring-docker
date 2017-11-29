f1a66f4c15851aab704722abfb0db379e41f4137
Vivesh Saladi <vivesh.saladi@momentum.co.za>
2017-11-29 12:46:32 +0200

    init

---
+++ src/main/java/sample/config/ConfigServer.java
@@ -0,0 +1,11 @@
+package sample.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+public class ConfigServer {
+}
